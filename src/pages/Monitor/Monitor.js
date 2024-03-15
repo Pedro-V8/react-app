5c6dcbc2a2a30939957725b66557af973925f03e
PedroV8 <pedrophdv@outlook.com>
2024-03-15 16:40:01 -0300

    Adding WS on Monitor

--- src/pages/Monitor/Monitor.js
+++ src/pages/Monitor/Monitor.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import './Monitor.css';
 import { useLocation } from 'react-router-dom';
 
@@ -6,7 +6,33 @@ function Monitor() {
   const location = useLocation();
   const client = location.state.client;
 
-  console.log(client)
+  const [websckt, setWebsckt] = useState(null);
+
+  useEffect(() => {
+    const url = "ws://localhost:8000/ws/" + 1;
+    const ws = new WebSocket(url);
+
+    ws.onopen = () => {
+      /*setInterval(() => {
+        ws.send(client);
+      }, 1000);*/
+      ws.send(JSON.stringify(client));
+    };
+
+    ws.onmessage = (e) => {
+      const message = JSON.parse(e.data);
+      console.log(message);
+    };
+
+    setWebsckt(ws);
+
+    // Clean up function when the component unmounts or before the effect runs again
+    return () => {
+      if (ws) {
+        ws.close();
+      }
+    };
+  }, []);
 
 
   return (
